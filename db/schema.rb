2f1643f808f2d64b5a5ea2f8d1bbdf39320aba89
yusaku916 <yy.yusaku916@ezweb.ne.jp>
2020-02-21 02:34:02 +0900

    登録情報にユーザー名追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_19_221434) do
+ActiveRecord::Schema.define(version: 2020_02_20_172623) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "theme"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_02_19_221434) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
