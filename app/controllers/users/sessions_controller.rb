7f5c7b2d07f3e66e340d992c54e4dd330471caf4
yusaku916 <yy.yusaku916@ezweb.ne.jp>
2020-03-02 22:11:52 +0900

    test

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,4 +1,4 @@
-frozen_string_literal: true
+# frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
   before_action :configure_sign_in_params, only: [:create]
