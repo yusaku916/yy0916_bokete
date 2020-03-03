668a97d31cd689542aaaf34bfda549e94b2d74f7
yusaku916 <yy.yusaku916@ezweb.ne.jp>
2020-03-03 23:42:03 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
   has_many :answers
+  validates :username, presence: true, length: { maximum: 6 }
 end
